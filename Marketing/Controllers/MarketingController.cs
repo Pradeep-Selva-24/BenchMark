5db433d4009805ac5274e8078eedc97169b7d9ec
pradeep <pradeepselva2024@gmail.com>
2023-08-30 13:58:49 +0530

    1st Commit#

---
+++ Marketing/Controllers/MarketingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marketing.Controllers
+{
+    public class MarketingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
